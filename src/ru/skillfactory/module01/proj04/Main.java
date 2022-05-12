3fdc999fd7a3806cd14afa34341f5f60b41b08e9
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-05-12 14:01:43 +0300

    Модуль 1. Задание 4

---
+++ src/ru/skillfactory/module01/proj04/Main.java
@@ -0,0 +1,37 @@
+package ru.skillfactory.module01.proj04;
+
+public class Main {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = 'b';
+        char c = 'c';
+        char d = 'd';
+        char e = 'e';
+        char f = 'f';
+        char g = 'g';
+        char h = 'h';
+        char i = 'i';
+        char j = 'j';
+        char k = 'k';
+        char l = 'l';
+        char m = 'm';
+        char n = 'n';
+        char o = 'o';
+        char p = 'p';
+        char q = 'q';
+        char r = 'r';
+        char s = 's';
+        char t = 't';
+        char u = 'u';
+        char v = 'v';
+        char w = 'w';
+        char x = 'x';
+        char y = 'y';
+        char z = 'z';
+
+        System.out.println("Мое имя " + n + i + k);
+
+        String result = "Мое имя " + n + i + k;
+        System.out.println(result);
+    }
+}
