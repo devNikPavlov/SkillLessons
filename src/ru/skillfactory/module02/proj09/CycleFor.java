872d4f46716ff844a14aec83ae9331a319f8e0b5
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-05-28 18:20:01 +0300

    Модуль 2. Задание 9

---
+++ src/ru/skillfactory/module02/proj09/CycleFor.java
@@ -0,0 +1,13 @@
+package ru.skillfactory.module02.proj09;
+
+/*
+Создайте цикл, выводящий на экран четные цифры из диапазона от 2 до 10 включительно
+ */
+
+public class CycleFor {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
