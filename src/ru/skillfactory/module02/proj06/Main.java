e001fe368a8ec7c86d4727fa3d9ceefc648e1962
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-05-28 18:08:03 +0300

    Модуль 2. Задание 6

---
+++ src/ru/skillfactory/module02/proj06/Main.java
@@ -0,0 +1,10 @@
+package ru.skillfactory.module02.proj06;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a <= 10) {
+            System.out.println(a++);
+        }
+    }
+}
