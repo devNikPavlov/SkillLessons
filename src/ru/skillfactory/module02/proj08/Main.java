dd1fd216ddcd2d3007833b5e02a399a79a50ea3f
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-05-28 18:14:49 +0300

    Модуль 2. Задание 8

---
+++ src/ru/skillfactory/module02/proj08/Main.java
@@ -0,0 +1,9 @@
+package ru.skillfactory.module02.proj08;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i +=10) {
+            System.out.println("Строка " + i);
+        }
+    }
+}
