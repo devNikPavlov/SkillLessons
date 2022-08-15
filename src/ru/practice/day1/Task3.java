cc0794047844c194ec0994e51791ce4286d7445a
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-08-15 17:36:42 +0300

    Day1, Задача 1.3

---
+++ src/ru/practice/day1/Task3.java
@@ -0,0 +1,14 @@
+package ru.practice.day1;
+
+/**
+ * @author Pavlov Nikolay
+ * Вывести задание 1 с помощью цикла for, но в столбик
+ */
+
+public class Task3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("JAVA ");
+        }
+    }
+}
