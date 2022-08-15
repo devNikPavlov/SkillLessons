3a1134b279abb48f85c7b8f49b99f0f2b684ddb6
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-08-15 17:48:19 +0300

    Day1, Задача 1.5

---
+++ src/ru/practice/day1/Task5.java
@@ -0,0 +1,17 @@
+package ru.practice.day1;
+
+/**
+ * @author Pavlov Nikolay
+ *
+ * Повторите задание 4, но используя цикл for
+ */
+
+public class Task5 {
+    public static void main(String[] args) {
+        int age = 23;
+
+        for (int i = age; i <= 100; i += 5) {
+            System.out.println("Мой возраст " + i);
+        }
+    }
+}
